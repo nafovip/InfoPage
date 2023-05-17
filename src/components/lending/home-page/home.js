7aae0835b7380a2dc0c669312b80cd8b846a37c8
nafovip <133018107+nafovip@users.noreply.github.com>
2023-05-17 11:00:43 +0200

    Update home.js

--- src/components/lending/home-page/home.js
+++ src/components/lending/home-page/home.js
@@ -30,19 +30,19 @@ export const Home = ({ siteTitle,isMobile }) => {
 								<a href="https://twitter.com/nafovip"><Icon name="tw" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="#"><Icon name="tg" fould="home" width={45} height={34} /></a>
+								<a href="https://t.me/nafovip"><Icon name="tg" fould="home" width={45} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="#"><Icon name="ds" fould="home" width={34} height={34} /></a>
+								<a href="https://discord.gg/UrR7uSNn"><Icon name="ds" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="#"><Icon name="dex" fould="home" width={34} height={34} /></a>
+								<a href="https://www.dextools.io/app/en/ether/pair-explorer/0x1e368d14ec562dcc27fad06cdb1a17d6a8facf34"><Icon name="dex" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="#"><Icon name="uni" fould="home" width={34} height={34} /></a>
+								<a href="https://app.uniswap.org/#/swap?outputCurrency=0xaf59f6613920d98ef64a133b21b28495a63e808f"><Icon name="uni" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="#"><Icon name="scan" fould="home" width={34} height={34} /></a>
+								<a href="https://etherscan.io/token/0xaf59f6613920d98ef64a133b21b28495a63e808f"><Icon name="scan" fould="home" width={34} height={34} /></a>
 							</li>
 						</ Fade>
 					</ul>
