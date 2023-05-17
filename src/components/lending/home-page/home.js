197123e749daaf30c4736529483f27b98662f5fe
nafovip <133018107+nafovip@users.noreply.github.com>
2023-05-17 15:58:32 +0200

    Update home.js

--- src/components/lending/home-page/home.js
+++ src/components/lending/home-page/home.js
@@ -27,16 +27,16 @@ export const Home = ({ siteTitle,isMobile }) => {
 					<ul class="soc-medias__links">
 						<Fade duration={2500}>
 							<li class="soc-medias__link">
-								<a href="https://t.me/nafovip" target="_blank"><Icon name="tw" fould="home" width={34} height={34} /></a>
+								<a href="https://twitter.com/nafovip" target="_blank"><Icon name="tw" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="https://discord.gg/UrR7uSNn" target="_blank"><Icon name="tg" fould="home" width={45} height={34} /></a>
+								<a href="https://t.me/nafovip" target="_blank"><Icon name="tg" fould="home" width={45} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="https://www.dextools.io/app/en/ether/pair-explorer/0x1e368d14ec562dcc27fad06cdb1a17d6a8facf34" target="_blank"><Icon name="ds" fould="home" width={34} height={34} /></a>
+								<a href="https://discord.gg/UrR7uSNn" target="_blank"><Icon name="ds" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
-								<a href="#"><Icon name="dex" fould="home" width={34} height={34} /></a>
+								<a href="https://www.dextools.io/app/en/ether/pair-explorer/0x1e368d14ec562dcc27fad06cdb1a17d6a8facf34" target="_blank"><Icon name="dex" fould="home" width={34} height={34} /></a>
 							</li>
 							<li class="soc-medias__link">
 								<a href="https://app.uniswap.org/#/swap?outputCurrency=0xaf59f6613920d98ef64a133b21b28495a63e808f" target="_blank"><Icon name="uni" fould="home" width={34} height={34} /></a>
